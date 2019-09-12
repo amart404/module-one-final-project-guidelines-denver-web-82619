cef506775cde02b393c12fb35245e204ec20858c
github username <github email address>
2019-09-12 10:08:20 -0600

    Finished MVP

--- lib/cli.rb
+++ lib/cli.rb
@@ -7,7 +7,7 @@ class Cli
         is_user = PROMPT.yes?("Are you a returning user?")
         if is_user
             usernames = User.all.map {|user| user.name}
-            prev_user = PROMPT.select("Select your team.", (usernames))
+            new_user = PROMPT.select("Select your team.", (usernames))
         else
             new_user = PROMPT.ask("Enter new team name: ", default: ENV['USER'])
             new_team = User.create(name: new_user)
@@ -44,28 +44,11 @@ class Cli
             team_dfns = Player.find {|player| player.name == new_dfns}
             Team.create(user: new_team, player: team_dfns)
         end
+        whole_team = Team.all.select{|team| team.user.name == new_user}
+        player_points = whole_team.map {|teammate| teammate.player.points}
+        total_points = (player_points.sum.to_f) / (16.0)
+        p total_points
     end
-
-    # def self.pick_team
-
-    #     qb = Player.where(position: "QB")
-    #     qb_names = qb.map {|player| player.name}.sort.uniq
-
-    #     rb = Player.where(position: "RB")
-    #     rb_names = rb.map {|player| player.name}.sort.uniq
-
-    #     wr = Player.where(position: "WR")
-    #     wr_names = wr.map {|player| player.name}.sort.uniq
-
-    #     dfns = Player.where(position: "Def")
-    #     dfns_names = dfns.map {|player| player.name}.sort.uniq
-
-    #     new_qb = PROMPT.select("Select your QB.", (qb_names))
-    #     new_rb = PROMPT.select("Select your RB.", (rb_names))
-    #     new_wr = PROMPT.select("Select your first WR.", (wr_names))
-    #     new_wr2 = PROMPT.select("Select your second WR.", (wr_names))
-    #     new_dfns = PROMPT.select("Select your Defense.", (dfns_names))
-    # end
 end
 
 
